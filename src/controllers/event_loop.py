d3ce7ccd22b4c9dd1a57459a8b84bd4bac60d22f
Joshua Gorman <90810000+endevii@users.noreply.github.com>
2023-11-07 09:58:18 -0500

    Basic window and event loop setup

---
+++ src/controllers/event_loop.py
@@ -0,0 +1,42 @@
+import sys
+
+import pygame
+from pygame import event
+
+# from models.player import Player
+from models.window import Window
+
+
+def event_loop() -> None:
+    """The main GUI event loop. Handles events then draws the screen. Loops until the user exits."""
+    print("Entering main game loop")
+    pygame.init()
+
+    window = Window()
+
+    # player = Player(0, window.width / 2 * .75)
+    # window.register_entity(player)
+
+    has_clicked = False
+
+    # Event loop: a loop that checks if new events have occurred and responds to them if they have
+    while True:
+        # check all the events sent from pygame
+        for _event in event.get():
+            # if a quit event was sent, end the process
+            if _event.type == pygame.QUIT:
+                print("Found QUIT event")
+                sys.exit()
+            # elif _event.type == pygame.MOUSEBUTTONDOWN:
+            #     if not has_clicked:
+            #         # TODO: Add a start menu, don't just leave gravity at 0 until the first click.
+            #         window.clear()
+            #         window.update()
+            #         player.gravity = 1500
+            #         has_clicked = True
+            #     else:
+            #         player.jump(window._frame_counter)
+
+        # handle the window
+        window.clear()
+        window.update()
---
+++ src/models/window.py
@@ -0,0 +1,79 @@
+from typing import List
+
+from pygame import Color, Surface, display, time, image
+
+from models.entity import Entity
+
+from models.background import Background
+
+DEFAULT_HEIGHT = 300
+DEFAULT_WIDTH = 420
+MARGIN_FACTOR = 0.9
+TARGET_FPS = 60
+
+INFO_ERROR = -1
+
+
+class Window:
+    """Represents the entire window that the game is rendered in."""
+    _window_surface: Surface = None
+    _entities: List[Entity] = []
+    _background: Background = None
+    _clock: time.Clock = time.Clock()
+
+    def __init__(self):
+        """Create a Window."""
+        # initialize the display module if it hasn't been already
+        display.init()
+        # Set the window size to be some fraction of the screen if we can get the screen size. If not,
+        # set it to some arbitrary default size
+        info = display.Info()
+        self._width = info.current_w * \
+            MARGIN_FACTOR if info.current_w != INFO_ERROR else DEFAULT_WIDTH
+        self._height = info.current_h * \
+            MARGIN_FACTOR if info.current_h != INFO_ERROR else DEFAULT_HEIGHT
+        self._window_surface = display.set_mode(
+            (int(self._width), int(self._height)))
+        self._frame_counter = 0
+        self._background = Background(image.load("assets/background.png"))
+        self._background.set_size(self._width, self._height)
+        self._background.set_window_surface(self._window_surface)
+
+    def clear(self):
+        """Clear all elements currently drawn to the window."""
+        self._window_surface.fill(Color(0, 0, 0))
+
+    def update(self):
+        """Updates the contents of the entire window."""
+        # tick the clock to ensure that the game runs at the target FPS
+        delta_time = self._clock.tick(TARGET_FPS) / 1000
+        self._background.draw()
+        for entity in self._entities:
+            # any motion should be multiplied by delta_time to ensure it isn't affected by framerate
+            entity.move(delta_time)
+            if entity.get_left() <= 0 or entity.get_right() >= self._width:
+                entity.handle_window_border_x()
+            if entity.get_top() <= 0 or entity.get_bottom() >= self._height:
+                entity.handle_window_border_y()
+            # blit means to actually draw the entity onto the screen
+            self._window_surface.blit(entity.get_surface(), entity.get_rect())
+        # flip redraws the window
+        display.flip()
+        # every update is a new frame
+        self._frame_counter += 1
+
+    def register_entity(self, entity: Entity):
+        """Register a new entity to be managed by the window."""
+        self._entities.append(entity)
+
+    def set_background(self, image: Surface):
+        self._background = Background(image)
+        self._background.set_size(self._width, self._height)
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def height(self):
+        return self._height
